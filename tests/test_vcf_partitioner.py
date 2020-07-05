16cbbd3c3786063f38283b162b4825c42418a281
davidtnfsh <davidtnfsh@gmail.com>
2020-07-05 19:54:33 +0800

    fix(fixtures): add scaffold fixtures into git

--- tests/test_vcf_partitioner.py
+++ tests/test_vcf_partitioner.py
@@ -5,7 +5,7 @@ from bio_partitioner.factory import PartitionerFactory
 
 def test_crawl_every_page():
     partitioner = PartitionerFactory.create_partitioner("vcf")
-    dataset = "fixtures/unphased.vcf"
+    dataset = "fixtures/scaffold.vcf"
     num_of_rows = partitioner.count_num_of_rows(dataset)
     partitioner.partition(dataset, num_of_rows, 10)
     if __debug__:
