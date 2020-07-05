bfb54f6bc71c8430da5b78fe1900153c1eb16e1e
davidtnfsh <davidtnfsh@gmail.com>
2020-07-05 20:01:01 +0800

    test(pytest): fix ut

--- tests/test_vcf_partitioner.py
+++ tests/test_vcf_partitioner.py
@@ -7,8 +7,8 @@ def test_crawl_every_page():
     partitioner = PartitionerFactory.create_partitioner("vcf")
     dataset = "fixtures/scaffold.vcf"
     num_of_rows = partitioner.count_num_of_rows(dataset)
-    partitioner.partition(dataset, num_of_rows, 10)
+    partitioner.partition(dataset, num_of_rows, 8)
     if __debug__:
-        for idx in range(10):
+        for idx in range(8):
             if not Path(f"{idx}.vcf").exists():
                 raise AssertionError(f"{idx}.vcf not found!")
